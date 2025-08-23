1428632ccebe2e96a9564988db851675c98e19e7
im-vedant <194vedantgupta@gmail.com>
2025-08-23 16:56:46 +0530

    chore: add compiler options to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  
+  "compilerOptions": {
+    "noImplicitAny": false
+  }
+
 };
 
 export default nextConfig;
